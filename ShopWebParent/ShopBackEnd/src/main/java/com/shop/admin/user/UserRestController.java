638d3b727ee846b47685f2146d215399cf5e7d9f
Asike <sapasask@gmail.com>
2022-06-26 11:43:12 +0600

    Added update user functionality

--- ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserRestController.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserRestController.java
@@ -2,7 +2,6 @@ package com.shop.admin.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,8 @@ public class UserRestController {
     private UserService service;
 
     @PostMapping("/users/check_email")
-    public String checkDuplicateEmail(@Param("email") String email) {
-        return service.isEmailUnique(email) ? "OK" : "Duplicated";
+    public String checkDuplicateEmail(@Param("id") Integer id,
+            @Param("email") String email) {
+        return service.isEmailUnique(id, email) ? "OK" : "Duplicated";
     }
 }
